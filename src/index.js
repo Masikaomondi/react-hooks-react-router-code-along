8ac8e52a4d22a8c26c2f0927800be49158799107
Masika <masikaomondi@gmail.com>
2022-09-28 00:30:52 +0300

    Completed assignment

--- src/index.js
+++ src/index.js
@@ -1,5 +1,9 @@
+// .src/index.js
+
 import React from "react";
 import ReactDOM from "react-dom";
+// Step 1\. Import react-router functions
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function Home() {
   return (
@@ -9,8 +13,12 @@ function Home() {
   );
 }
 
-function App() {
-  return <Home />;
-}
-
-ReactDOM.render(<App />, document.getElementById("root"));
+// Step 2\. Change so router is coordinating what is displayed
+ReactDOM.render(
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Home />} />
+    </Routes>
+  </BrowserRouter>,
+  document.getElementById("root")
+);
